5a5f978efb2d85b1ddec4a1a73764650c5d24873
Kai <90447061+Heyaaman-UB@users.noreply.github.com>
2021-11-03 21:42:16 +0530

    Update private.py

--- DaisyXMusic/modules/private.py
+++ DaisyXMusic/modules/private.py
@@ -43,9 +43,9 @@ def _start(client, message):
                InlineKeyboardButton("Chat Group", url="https://t.me/international_english_chattings")
             ],
             [
-               InlineKeyboardButton("➕ Add me to your Group ", url="https://t.me/Phoenix_Music_robot?startgroup=true")
-               InlineKeyboardButton(
-                   text="Help commands", url="http://t.me/Phoenix_Music_robot?start=help")
+               InlineKeyboardButton("➕ Add me to your Group ", url="https://t.me/Phoenix_Music_robot?startgroup=true"
+               ),
+               InlineKeyboardButton("Help commands", url="http://t.me/Phoenix_Music_robot?start=help")
            ]]
         ),
         reply_to_message_id=message.message_id,
